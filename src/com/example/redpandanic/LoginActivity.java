f2a65fc196291789807dff56c8617c33fec4611f
Kevin Rivera <kevinrivera427@gmail.com>
2014-11-29 05:49:15 +0800

    a

--- src/com/example/redpandanic/LoginActivity.java
+++ src/com/example/redpandanic/LoginActivity.java
@@ -1,11 +1,19 @@
 package com.example.redpandanic;
 
+import java.util.List;
+
 import com.example.redpandanic.ItemRecommenderActivity.RecommendListener;
+import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
+import com.microsoft.windowsazure.mobileservices.MobileServiceTable;
+import com.microsoft.windowsazure.mobileservices.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.TableQueryCallback;
 
 import Database.DbConnection;
+import Model.Member;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -13,13 +21,61 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 public class LoginActivity extends Activity {
+	private MobileServiceClient mClient;
 	private Button btnRegister;
+	private Button btnLogin;
+	private EditText txtUsername;
+	private EditText txtPassword;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		mClient = DbConnection.connectToAzureService(this);
 		btnRegister = (Button)findViewById(R.id.registerButton);
 		btnRegister.setOnClickListener(new registerListener());
+		
+		btnLogin = (Button)findViewById(R.id.Login);
+		
+		txtUsername = (EditText)findViewById(R.id.txtUser);
+		txtPassword = (EditText)findViewById(R.id.txtPassword);
+	}
+	
+	class loginListener implements OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			String username = txtUsername.getText().toString();
+			String password = txtPassword.getText().toString();
+			MobileServiceTable<Member> memberTable = mClient.getTable(Member.class);
+			
+			memberTable.where().field("username").eq(username).and()
+			.field("password").eq(password)
+			.execute(new TableQueryCallback<Member>(){
+
+				@Override
+				public void onCompleted(List<Member> member, int position,
+						Exception exception, ServiceFilterResponse response) {
+					if(exception!=null){
+						if (member.size() == 0) {
+							Log.e("Login","Failure");
+						}
+						else{
+							Log.e("Login","Success");
+							Member mem = member.get(0);
+							Log.e("Login",mem.getMemberId());
+							if(mem.getGroupId()==null){
+								//Go to...
+							}
+							else{
+								//Do something else
+							}
+						}
+					}
+				}
+			});
+		}
+		
 	}
 	class registerListener implements OnClickListener{
 
