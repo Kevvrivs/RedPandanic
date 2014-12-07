b7be086a3a99eb12053b138de126259b6f4611f7
JovenCruz <jovencruz23@gmail.com>
2014-12-07 10:39:06 +0800

    Updated Layouts + Inventory and Member Layout + Member Adapter
    I did the Member Layout and Adapter but I don't know kung papaano yung
    connection sa Azure.
    
    I made Inventory Layout as a View Module muna.
    
    Okay na yung Manifest, hindi na Login and Android logo yung app natin,
    haha.

---
+++ src/Adapter/MemberAdapter.java
@@ -0,0 +1,56 @@
+package Adapter;
+
+import com.example.redpandanic.R;
+import com.example.redpandanic.R.id;
+
+import Model.Member;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class MemberAdapter extends ArrayAdapter<Member> {
+	
+	/**
+	 * Adapter context
+	 */
+	private Context mContext;
+
+	/**
+	 * Adapter View layout
+	 */
+	private int mLayoutResourceId;
+	
+	public MemberAdapter(Context context, int layoutResourceId) {
+		super(context, layoutResourceId);
+
+		mContext = context;
+		mLayoutResourceId = layoutResourceId;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View row = convertView;
+
+		final Member currentMember = getItem(position);
+
+		if (row == null) {
+			LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+			row = inflater.inflate(mLayoutResourceId, parent, false);
+		}
+
+		row.setTag(currentMember);
+		final TextView Name = (TextView)row.findViewById(R.id.textName);
+		final TextView Username = (TextView)row.findViewById(R.id.textUserName);
+		final TextView Email = (TextView) row.findViewById(R.id.textEmail);
+		Name.setText(currentMember.getMemberName());
+		Username.setText(currentMember.getUsername());
+		Email.setText(currentMember.getEmail());
+		
+
+		return row;
+	}
+}
